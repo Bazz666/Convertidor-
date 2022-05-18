d0cf0cd9aa45a48e861b830fe117b4b7a5ac5844
bazz666 <herrera.se1992@gmail.com>
2022-05-18 14:11:11 -0400

    heroku

--- public/excel.js
+++ public/excel.js
@@ -28,6 +28,18 @@ document.getElementById("button").addEventListener("click", () => {
 
         workbook.SheetNames.forEach((sheet) => {
             let rowObject = XLSX.utils.sheet_to_json(workbook.Sheets[sheet]);
+            //guardando rbd
+            let rbds = [];
+            rowObject.map((item) => {
+            
+                
+            
+        
+                rbds.push(item.RBD);
+            });
+            console.log(rbds[0])
+            
+
             //Limpiando datos
             let nData = rowObject.map(function (item) {
             if (item["Cod Tipo Enseñanza"] == 310) {
@@ -37,6 +49,7 @@ document.getElementById("button").addEventListener("click", () => {
             } else {
                 item.Nivel = "Primaria";
             }
+            
             delete item["Cod Tipo Enseñanza"];
             delete item["Fecha Incorporación Curso"];
             delete item["Código Etnia"];
@@ -60,8 +73,38 @@ document.getElementById("button").addEventListener("click", () => {
             item.RUT = item.Run + "-" + item["Dígito Ver."];
             delete item["Dígito Ver."];
             delete item.Run;
-            // Desc_grado to " Grado"
+            // Desc_grado to "Grado"
             item.Grado = item["Desc Grado"];
+            //formato Grado
+            if (item.Grado == "1° medio") {
+                item.Grado = "1º- Medio";
+            } else if (item.Grado == "2° medio") {
+                item.Grado = "2º- Medio";
+            } else if (item.Grado == "3° medio") {
+                item.Grado = "3º- Medio";
+            } else if (item.Grado == "4° medio") {
+                item.Grado = "4º- Medio";
+            } else if (item.Grado == "1° básico") {
+                item.Grado = "1º- Básico";
+            } else if (item.Grado == "2° básico") {
+                item.Grado = "2º- Básico";
+            } else if (item.Grado == "3° básico") {
+                item.Grado = "3º- Básico";
+            } else if (item.Grado == "4° básico") {
+                item.Grado = "4º- Básico";
+            } else if (item.Grado == "5° básico") {
+                item.Grado = "5º- Básico";
+            } else if (item.Grado == "6° básico") {
+                item.Grado = "6º- Básico";
+            } else if (item.Grado == "7° básico") {
+                item.Grado = "7º- Básico";
+            } else if (item.Grado == "8° básico") {
+                item.Grado = "8º- Básico";
+
+            } else {
+                item.Grado = "pendiente";
+            }
+            
             delete item["Desc Grado"];
             //Letra curso to Grupo
             item.Grupo = "Grupo " + item["Letra Curso"];
@@ -87,7 +130,7 @@ document.getElementById("button").addEventListener("click", () => {
             object.Nombre = item.Nombre;
             object["Apellido Paterno"]= item["Apellido Paterno"];
             object["Apellido Materno"]= item["Apellido Materno"];
-            object.Genero= item.Genero;
+            object.Sexo= item.Genero;
             object["Fecha de Nacimiento"]= item["Fecha Nacimiento"];
             object.Login= item.Login;
             object.Password= item.Password;
@@ -101,8 +144,8 @@ document.getElementById("button").addEventListener("click", () => {
             var ws = XLSX.utils.json_to_sheet(orderData);
 
             var wb = XLSX.utils.book_new();
-            XLSX.utils.book_append_sheet(wb, ws, "test");
-            XLSX.writeFile(wb, "test" + ".xlsx");
+            XLSX.utils.book_append_sheet(wb, ws, "Plantilla alta de alumnos");
+            XLSX.writeFile(wb, "Colegio_RBD_"+rbds[0]+ ".xlsx");
         });
 
         document.getElementById("button").addEventListener("click", traerDatos);
@@ -111,17 +154,17 @@ document.getElementById("button").addEventListener("click", () => {
           // res.innerHTML = ''; //ciclo for
             for (let item of orderData) {
             res.innerHTML += `<tr>
-                        <td>${item.Nivel}</td>
-                        <td>${item.Grado}</td>
-                        <td>${item.Grupo}</td>
-                        <td>${item.Nombre}</td>
-                        <td>${item["Apellido Paterno"]}</td>
-                        <td>${item["Apellido Materno"]}</td>
-                        <td>${item.Genero}</td>
-                        <td>${item["Fecha de Nacimiento"]}</td>
-                        <td>${item.Login}</td>
-                        <td>${item.Password}</td>
-                    <tr>`;
+                    <td>${item.Nivel}</td>
+                    <td>${item.Grado}</td>
+                    <td>${item.Grupo}</td>
+                    <td>${item.Nombre}</td>
+                    <td>${item["Apellido Paterno"]}</td>
+                    <td>${item["Apellido Materno"]}</td>
+                    <td>${item.Sexo}</td>
+                    <td>${item["Fecha de Nacimiento"]}</td>
+                    <td>${item.Login}</td>
+                    <td>${item.Password}</td>
+                <tr>`;
             }
         }
     });};}
