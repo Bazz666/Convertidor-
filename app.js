95c6f09e0ce9f1454393ee6cb2ad15d2e6661810
bazz666 <herrera.se1992@gmail.com>
2022-05-18 15:20:21 -0400

    fix routes

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ var engines = require('consolidate');
 app.engine('html', engines.mustache);
 app.set('view engine', 'html');
 
+app.listen(3000, () => console.log("Your app listening on port 3000"));
 
 // app.use(express.static('public'));
 // app.get('/', function(req, res) {
