2186cdb82ac736ad1de9354622ed51667d8d88e1
bazz666 <herrera.se1992@gmail.com>
2022-05-23 13:10:52 -0400

    heroku deploy

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 // settings
-app.set("port", process.env.PORT || 8080);
+app.set("port", process.env.PORT || 5000);
 app.set("views", path.join(__dirname, "views"));
 // app.set("view engine", "ejs");
 // app.engine('html', engines.mustache);
@@ -31,6 +31,6 @@ app.use(express.static(path.join(__dirname, "public")));
 // app.listen(app.get("port"), () => {
 //   console.log("Server on port", app.get("port"));
 // });
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 5000);
 
 // module.exports = app;
