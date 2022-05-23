17e837779bacd7f66383f3b6c30c882a9ce73a0a
bazz666 <herrera.se1992@gmail.com>
2022-05-23 12:28:30 -0400

    order a project and dependencies

---
+++ src/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+// var createError = require('http-errors');
+const express = require('express');
+const app = express();
+const path = require('path');
+// settings
+app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "ejs");
+// app.engine('html', engines.mustache);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// Body Parcer
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+// Logger middleware
+const logger = require('morgan');
+app.use(logger('dev'));
+
+//Routes
+app.use(require("./routes"));
+// static files
+app.use(express.static(path.join(__dirname, "public")));
+
+// listening the Server
+app.listen(app.get("port"), () => {
+  console.log("Server on port", app.get("port"));
+});
+
+// module.exports = app;
