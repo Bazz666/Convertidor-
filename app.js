289eaf6b52f37150df1fd64f6150024bd79d60fb
bazz666 <herrera.se1992@gmail.com>
2022-05-23 15:30:05 -0400

    aswer

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 // settings
-// app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 8080));
 app.set("views", path.join(__dirname, "views"));
 // app.set("view engine", "ejs");
 // app.engine('html', engines.mustache);
@@ -35,8 +35,8 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // module.exports = app;
 
-// app.listen(app.get("port"), () => {
-//   console.log("Server on port", app.get("port"));
-// });
+app.listen(app.get("port"), () => {
+  console.log("Server on port", app.get("port"));
+});
 
-app.listen(process.env.PORT || 8080);
+// app.listen(process.env.PORT || 8080);
