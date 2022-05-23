2a14eac67cee3fc2e561a34c20f34d324afa3f6d
bazz666 <herrera.se1992@gmail.com>
2022-05-23 15:23:54 -0400

    port

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 // settings
-app.set('port', (process.env.PORT || 5000));
+// app.set('port', (process.env.PORT || 5000));
 app.set("views", path.join(__dirname, "views"));
 // app.set("view engine", "ejs");
 // app.engine('html', engines.mustache);
