9051df6268c65c823984b4fe23ba1ccf988a2d7c
bazz666 <herrera.se1992@gmail.com>
2022-05-23 15:45:25 -0400

    wip

--- app.js
+++ app.js
@@ -5,10 +5,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 // settings
-app.set('port', (process.env.PORT || 8080));
-app.set("views", path.join(__dirname, "views"));
-// app.set("view engine", "ejs");
-// app.engine('html', engines.mustache);
+app.set('port', (process.env.PORT || 3000));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
@@ -22,19 +19,11 @@ app.use(bodyParser.urlencoded({
 const logger = require('morgan');
 app.use(logger('dev'));
 
-//Routes
-app.use(require("./src/routes"));
+//routes
+app.use(require("./routes"));
 // static files
 app.use(express.static(path.join(__dirname, "public")));
-
-// listening the Server
-// app.listen(app.get("port"), () => {
-//   console.log("Server on port", app.get("port"));
-// });
-// app.listen(process.env.PORT || 5000);
-
 // module.exports = app;
-
 app.listen(app.get("port"), () => {
   console.log("Server on port", app.get("port"));
 });
