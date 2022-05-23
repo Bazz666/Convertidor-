f90705eea8ebde44eb3e27fe822d26cf3b868c6a
bazz666 <herrera.se1992@gmail.com>
2022-05-23 13:19:58 -0400

    heroku ports

--- src/app.js
+++ src/app.js
@@ -31,6 +31,10 @@ app.use(express.static(path.join(__dirname, "public")));
 // app.listen(app.get("port"), () => {
 //   console.log("Server on port", app.get("port"));
 // });
-app.listen(process.env.PORT || 5000);
+// app.listen(process.env.PORT || 5000);
 
 // module.exports = app;
+
+app.listen(app.get("port"), () => {
+  console.log("Server on port", app.get("port"));
+});
